3b794c750b3e9cf600cae6c27fd4142467f750f4
Adam Kovacs <kovadam98@gmail.com>
2025-04-05 11:48:58 +0800

    feat: add AppleTeamService Get

--- internal/api/appleteam/get_test.go
+++ internal/api/appleteam/get_test.go
@@ -1,1 +1,53 @@
 package appleteam
+
+import (
+	"testing"
+
+	"github.com/fintreal/eas-sdk-go/internal/graphql"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestGetByIdentifier(t *testing.T) {
+	expectedData := &AppleTeamData{
+		Id:         "test-id",
+		Name:       "test-name",
+		Identifier: "test-identifier",
+		Type:       "test-type",
+		AccountId:  "test-account-id",
+	}
+
+	expectedVariables := map[string]any{
+		"identifier": expectedData.Identifier,
+		"accountId":  expectedData.AccountId,
+	}
+
+	graphQLMock := newGetGraphQLMock(expectedData)
+
+	service := NewAppleTeamService(graphQLMock)
+
+	actualData, actualErr := service.GetByIdentifier(expectedData.Identifier, expectedData.AccountId)
+
+	actualQuery := graphQLMock.Calls[0].Arguments.Get(0).(string)
+	actualVariables := graphQLMock.Calls[0].Arguments.Get(1).(map[string]any)
+
+	assert.Equal(t, getAppleTeamByIdentifierQuery, actualQuery)
+	assert.Equal(t, expectedVariables, actualVariables)
+	assert.Equal(t, expectedData, actualData)
+	assert.Equal(t, nil, actualErr)
+}
+
+func newGetGraphQLMock(data *AppleTeamData) *graphql.GraphQLMock {
+	mockData := &appleTeamData{
+		Id:         data.Id,
+		Name:       data.Name,
+		Identifier: data.Identifier,
+		Type:       data.Type,
+		Account:    account{Id: data.AccountId},
+	}
+	graphQLMock := graphql.NewGraphQLMock()
+	graphQLMock.On("Query", mock.Anything, mock.Anything, mock.Anything).Run(func(args mock.Arguments) {
+		*args.Get(2).(*getAppleTeamResponse) = getAppleTeamResponse{GetAppleTeam: getAppleTeam{Data: mockData}}
+	}).Return(nil)
+	return graphQLMock
+}

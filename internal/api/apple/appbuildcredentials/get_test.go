69333e52a549fc8d9b3c2c640e3f62bcbfd9f313
Adam Kovacs <kovadam98@gmail.com>
2025-04-08 21:29:11 +0800

    feat: appbuildcredentials

---
+++ internal/api/apple/appbuildcredentials/get_test.go
@@ -0,0 +1,56 @@
+package appbuildcredentials
+
+import (
+	"testing"
+
+	"github.com/fintreal/eas-sdk-go/internal/utils"
+)
+
+func TestGetByIdentifier(t *testing.T) {
+	input := &GetData{
+		Id:    "test-id",
+		AppId: "test-app-id",
+	}
+	expectedData := &Data{
+		Id:                    input.Id,
+		DistributionType:      "APP_STORE",
+		CertificateId:         "test-certificate-id",
+		ProvisioningProfileId: "test-provisoning-profile-id",
+		AppCredentialsId:      "test-app-credentials-id",
+	}
+
+	expectedVariables := map[string]any{
+		"appId": input.AppId,
+	}
+
+	mockResponse := getResponse{
+		AppByAppId: appByAppId{
+			IosAppCredentials: iosAppCredentials{
+				Data: []data{{
+					Id:               expectedData.Id,
+					DistributionType: expectedData.DistributionType,
+					ProvisioningProfile: objWithId{
+						Id: expectedData.ProvisioningProfileId,
+					},
+					Certificate: objWithId{
+						Id: expectedData.CertificateId,
+					},
+					AppCredentials: objWithId{
+						Id: expectedData.AppCredentialsId,
+					},
+				}},
+			},
+		},
+	}
+
+	config := utils.TestConfig[GetData, Data, getResponse, Service]{
+		NewServiceFunction: NewService,
+		FunctionUnderTest:  "Get",
+		Input:              input,
+		MockResponse:       mockResponse,
+		ExpectedQuery:      getQuery,
+		ExpectedVariables:  expectedVariables,
+		ExpectedData:       expectedData,
+	}
+	utils.Test(t, config)
+}

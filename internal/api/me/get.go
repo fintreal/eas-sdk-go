f40841208d7d5f2ec5ab7a1576b2ddf52a1333c2
Adam Kovacs <kovadam98@gmail.com>
2025-04-08 10:03:12 +0800

    refactor: me get

--- internal/api/me/get.go
+++ internal/api/me/get.go
@@ -1,5 +1,7 @@
 package me
 
+import "github.com/fintreal/eas-sdk-go/internal/utils"
+
 type getResponse struct {
 	Data *Data `json:"meActor"`
 }
@@ -14,9 +16,19 @@ const getQuery = `
 
 // Retrieves the current user name and id from EAS
 func (service *service) Get() (*Data, error) {
-	variables := map[string]any{}
+	inputMapper := func(a any) map[string]any {
+		return map[string]any{}
+	}
 
-	var response getResponse
-	err := service.graphql.Query(getQuery, variables, &response)
-	return response.Data, err
+	outputMapper := func(response getResponse, _ any) *Data {
+		return response.Data
+	}
+
+	config := utils.Configuration[any, Data, getResponse]{
+		Query:        getQuery,
+		InputMapper:  inputMapper,
+		OutputMapper: outputMapper,
+	}
+
+	return utils.ConfigurationFunc(config, service.graphql)(nil)
 }

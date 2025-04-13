800d6ec0129c418071355ab500f25ad1db5979a2
Adam Kovacs <kovadam98@gmail.com>
2025-04-13 08:52:57 +0800

    test: add unit tests

---
+++ internal/api/android/appcredentials/create_test.go
@@ -0,0 +1,54 @@
+package appcredentials
+
+import (
+	"testing"
+
+	"github.com/fintreal/eas-sdk-go/internal/utils"
+)
+
+func TestCreate(t *testing.T) {
+	expectedData := &Data{
+		Id:                        "test-id",
+		AppId:                     "test-app-id",
+		Identifier:                "test-identifier",
+		GoogleServiceAccountKeyId: "test-google-service-account-key-id",
+	}
+
+	expectedVariables := map[string]any{
+		"appId":                     expectedData.AppId,
+		"identifier":                expectedData.Identifier,
+		"googleServiceAccountKeyId": expectedData.GoogleServiceAccountKeyId,
+	}
+
+	mockResponse := createResponse{
+		AndroidAppCredentials: createAndroidAppCredentials{
+			Data: data{
+				Id: expectedData.Id,
+				App: objWithId{
+					Id: expectedData.AppId,
+				},
+				Identifier: expectedData.Identifier,
+				GoogleServiceAccountKey: objWithId{
+					Id: expectedData.GoogleServiceAccountKeyId,
+				},
+			},
+		},
+	}
+
+	input := &CreateData{
+		AppId:                     expectedData.AppId,
+		Identifier:                expectedData.Identifier,
+		GoogleServiceAccountKeyId: expectedData.GoogleServiceAccountKeyId,
+	}
+
+	config := utils.TestConfig[CreateData, Data, createResponse, Service]{
+		NewServiceFunction: NewService,
+		FunctionUnderTest:  "Create",
+		Input:              input,
+		MockResponse:       mockResponse,
+		ExpectedQuery:      createQuery,
+		ExpectedVariables:  expectedVariables,
+		ExpectedData:       expectedData,
+	}
+	utils.Test(t, config)
+}
